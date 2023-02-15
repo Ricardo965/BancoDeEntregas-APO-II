7f3360d5fa29dc35a002b09f8f31eed78ee7814d
Ricardo965 <ricardiox2000@gmail.com>
2023-02-15 09:17:11 -0500

    Primer commit

---
+++ Node.java
@@ -0,0 +1,31 @@
+package CircularLists;
+
+public class Node {
+
+    private String value;
+    private Node next;
+    private Node previous;
+
+    public Node(String value) {
+        this.value = value;
+    }
+    public String getValue() {
+        return value;
+    }
+    public void setValue(String value) {
+        this.value = value;
+    }
+    public Node getNext() {
+        return next;
+    }
+    public void setNext(Node next) {
+        this.next = next;
+    }
+    public Node getPrevious() {
+        return previous;
+    }
+    public void setPrevious(Node previous) {
+        this.previous = previous;
+    }
+    
+}
